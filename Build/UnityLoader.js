52aa48d337f587815182d25bbb0576b166e64228
BlazJurisic <jurisic.blaz@gmail.com>
2020-06-29 16:12:47 +0200

    fixing canvas size

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -778,18 +778,19 @@ var UnityLoader = UnityLoader || {
 
   })(),
   compatibilityCheck: function (unityInstance, onsuccess, onerror) {
-    if (!UnityLoader.SystemInfo.hasWebGL) {
-      unityInstance.popup("Your browser does not support WebGL",
-        [{text: "OK", callback: onerror}]);
-    } else if (UnityLoader.SystemInfo.mobile) {
-      unityInstance.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.",
-        [{text: "OK", callback: onsuccess}]);
-    } else if (["Edge", "Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1) {
-      unityInstance.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.",
-        [{text: "OK", callback: onsuccess}]);
-    } else {
-      onsuccess();
-    }
+    // if (!UnityLoader.SystemInfo.hasWebGL) {
+    //   unityInstance.popup("Your browser does not support WebGL",
+    //     [{text: "OK", callback: onerror}]);
+    // } else if (UnityLoader.SystemInfo.mobile) {
+    //   unityInstance.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.",
+    //     [{text: "OK", callback: onsuccess}]);
+    // } else if (["Edge", "Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1) {
+    //   unityInstance.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.",
+    //     [{text: "OK", callback: onsuccess}]);
+    // } else {
+    //   onsuccess();
+    // }
+    onsuccess()
     
   },
   buildCompatibilityCheck: function(Module, onsuccess, onerror) {
